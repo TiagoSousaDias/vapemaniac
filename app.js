2aade2d4798496fc862109c4d8ee875fc0d1a436
unknown <TyranoPT>
2018-10-25 13:03:40 +0100

    Add Public folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
-
 const port = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cors);
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/index.html'));
+});
 
 app.listen(port,(err)=>{
   if(err){
