0d09f34a9112baca76e9ab007ee0c5ff142b99b5
Tiago Dias <tiago024@gmail.com>
2018-11-12 22:25:59 +0000

    Testing routes again

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 
+
+//MIddlewares
+app.use(cors());
+app.use(bodyParser.json());
+//Static Folder
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use(cors);
+app.use('/',require('./routes/routes').router);
 
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'public/index.html'));
