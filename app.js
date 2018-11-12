96a9ae0730ce5a537db1c28b162f31986a19c8b8
Tiago Dias <tdias_benfiquista@hotmail.com>
2018-11-12 12:41:30 +0000

    Correcting routes

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(cors);
+//Define all Routes
+app.use('/',require('./routes/routes').router);
 
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'public/index.html'));
 });
-//Define all Routes
-app.use('/',require('./routes/routes').router);
+
 
 app.listen(port,(err)=>{
   if(err){
---
+++ routes/mystore.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+//Routes go Here
+
+router.get('/',(req,res)=>{
+
+  res.send('Backoffice login');
+});
+
+module.exports = router;
