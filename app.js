f5602c39e42ffd5d77e92f0d852a005c7f8564aa
Tiago Dias <tdias_benfiquista@hotmail.com>
2018-11-12 17:27:36 +0000

    go back houston

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors);
+
 app.use(express.static(path.join(__dirname,'public')));
-app.use(bodyParser.json());
+
+app.use(cors);
 
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'public/index.html'));
-//  if (process.env.NODE_ENV === 'production') res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-//Define all Routes
-//app.use('/',require('./routes/routes').router);
-
 
 app.listen(port,(err)=>{
   if(err){
