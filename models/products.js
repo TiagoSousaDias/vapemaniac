8d996cbb789da985ae167abcc0661f20056b5902
Tiago Dias <tiago024@gmail.com>
2018-11-13 00:12:40 +0000

    Last Test

--- models/products.js
+++ models/products.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const config = require('../config/configs');
 module.exports.getProducts=(callback)=>{
-pg.connect(connectionString, function(err, client, done) {
+pg.connect(config.database.host, function(err, client, done) {
    client.query("SELECT table_name  FROM information_schema.tables  WHERE table_schema='public' AND table_type='BASE TABLE;" , function(err, result) {
       done();
       if(err) return console.error(err);
