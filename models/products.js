4ed7e79a41d0e31b676c24fcd25225ccb49f7e20
Tiago Dias <tiago024@gmail.com>
2018-11-13 10:47:46 +0000

    PG test

--- models/products.js
+++ models/products.js
@@ -1,11 +1,14 @@
-const pg = require('pg');
+const  { Pool, Client } = require('pg');
+const pool = new Pool({
+  connectionString:config.database.host,
+})
 const config = require('../config/configs');
 module.exports.getProducts=(callback)=>{
-pg.connect(config.database.host, function(err, client, done) {
-   client.query("SELECT table_name  FROM information_schema.tables  WHERE table_schema='public' AND table_type='BASE TABLE;" , function(err, result) {
+
+   pool.query("SELECT table_name  FROM information_schema.tables  WHERE table_schema='public' AND table_type='BASE TABLE;" , function(err, result) {
       done();
       if(err) return console.error(err);
       return callback(result.rows);
    });
-});
+
 };
