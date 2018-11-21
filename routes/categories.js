4a34ccc53ce75badd07a6cc8a098af688d44800f
Tiago Dias <tiago024@gmail.com>
2018-11-21 18:33:20 +0000

    teste

--- routes/categories.js
+++ routes/categories.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Categories = require('../models/categories');
 
+router.get('/',(req,res)=>{
+  Categories.getCategories((results)=>{
+    res.json({results});
+  });
+});
+
 router.get('/catMenu',(req,res)=>{
   Categories.getMenuCategories((results)=>{
     res.json({results});
   });
 });
 
-router.get('/',(req,res)=>{
-  Categories.getCategories((results)=>{
-    res.json({results});
-  });
-});
+
 
 module.exports.router = router;
